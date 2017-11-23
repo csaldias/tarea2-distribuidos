603bd224965bc068d8ceccb4559d5f409e6c7615
Camilo Saldías <camilo.saldias@gmail.com>
2017-11-23 16:40:31 -0300

    Añadido TODO de Interfaces a implementar.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tarea2-distribuidos
-Tarea 2 del ramo de Sistemas Distribuídos 2017-2
+# Tarea 2 INF-343 2017-2
+Tarea 2 del ramo de Sistemas Distribuídos 2017-2.
--- src/Interfaz.java
+++ src/Interfaz.java
@@ -6,3 +6,17 @@ public interface Interfaz extends Remote {
     public String helloTo(String name) throws RemoteException;
 
 }
+
+/*TODO: Implementar:
+    request(id, seq):
+        Método que registra un request de un proceso remoto. Recibe el id del proceso que hace la petición y el
+        número de petición del proceso.
+    waitToken():
+        Método que le indica a un proceso remoto que debe esperar por el token para realizar la sección
+        crítica.
+    takeToken(token):
+        Método que toma posesión del token en el proceso.
+    kill():
+        Método que mata el proceso remoto. Debe usar este método para detener el algoritmo de S-K una vez que el
+        token haya pasado por todos los nodos del sistema.
+    */
